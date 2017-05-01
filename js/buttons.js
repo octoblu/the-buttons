fef55e4d62a8d3e31227837290e2af5722544067
Peter DeMartini <thepeterdemartini@gmail.com>
2017-05-01 09:01:28 -0700

    v1.0.0 upgraded things

--- js/buttons.js
+++ js/buttons.js
@@ -1,64 +1,78 @@
 'use strict';
-var buttonsStart = function(uuid, token){
-  $('#loading-spinner').slideDown();
-  var connection = meshblu.createConnection({
-    uuid: uuid,
-    token: token
-  });
-
-  connection.on('ready', function(data){
-    console.log('ready');
-    connection.whoami({}, function(device){
-      $('#loading-spinner').slideUp();
-      console.log('whoami', device);
-      if(device.error){
-        return console.error('error getting device', device.error);
+var loadButtonsInterval = null;
+function loadButtons(meshbluHttp){
+  meshbluHttp.whoami(function(error, device){
+    if(error) {
+      return console.error('whoami error', error)
+    }
+    $('#page-title').text(device.name);
+    $('#buttons-container').html("");
+    if (!device.buttons || !device.buttons.length) {
+      $('#no-buttons').show();
+      device.buttons = []
+    } else {
+      $('#no-buttons').hide()
+    }
+    device.buttons.forEach(function(button){
+      var template = "";
+      if(!button.href && !button.topic){
+        return;
       }
-      $('#page-title').text(device.name);
-      $('#buttons-container').html("");
-      device.buttons.forEach(function(button){
-        var template = "";
-        if(!button.href && !button.topic){
-          return;
-        }
-        var color = button.color || "teal";
-        if(button.href){
-          template = $($('#link-template').html());
-          template.attr('href', button.href);
-        }else{
-          template = $($('#button-template').html());
-          template.attr('data', JSON.stringify(button));
-        }
-        template.text(button.name);
-        template.addClass(color);
-        button.element = $('#buttons-container').append(template);
-
-      });
-      $('button').click(function(event){
-        event.preventDefault();
-        var element = $(this);
-        if(element.is('a')){
-          window.location= element.attr('href');
-        }else{
-          var message = JSON.parse(element.attr('data'));
-          message.devices = '*';
-          connection.message(message);
-        }
-      });
+      var color = button.color || "teal";
+      if(button.href){
+        template = $($('#link-template').html());
+        template.attr('href', button.href);
+      }else{
+        template = $($('#button-template').html());
+        template.attr('data', JSON.stringify(button));
+      }
+      template.text(button.name);
+      template.addClass(color);
+      button.element = $('#buttons-container').append(template);
+    });
+    $('button').off('click')
+    $('button').on('click', function(event){
+      event.preventDefault();
+      var element = $(this);
+      if(element.is('a')){
+        window.location= element.attr('href');
+      }else{
+        var message = JSON.parse(element.attr('data'));
+        message.devices = '*';
+        meshbluHttp.message(message, function(error) {
+          if(error) {
+            return console.error('message error', error)
+          }
+        });
+      }
     });
   });
 };
+
+function loadButtonsOnInterval(uuid, token) {
+  clearInterval(loadButtonsInterval)
+  var meshbluHttp = new MeshbluHttp({
+    uuid: uuid,
+    token: token,
+  })
+  $('#loading-spinner').hide()
+  loadButtons(meshbluHttp)
+  loadButtonsInterval = setInterval(function() {
+    loadButtons(meshbluHttp)
+  }, 1000 * 2)
+}
 
 $(function(){
   var lastUuid, lastToken;
   var interval;
   var checkForCreds = function(){
-    var uuid = location.hash.substring(2).split('/')[0];
-    var token = location.hash.substring(2).split('/')[1];
+    var hashItems = location.hash.substring(2).split('/')
+    var uuid = hashItems[0];
+    var token = hashItems[1];
     if(uuid && token && uuid !== lastUuid && token !== lastToken){
       lastUuid = uuid;
       lastToken = token;
-      buttonsStart(uuid, token);
+      loadButtonsOnInterval(uuid, token);
     }
   };
   interval = setInterval(checkForCreds, 1000);
